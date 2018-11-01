b0537f24357c43c424b823b7781dcb85f52a9e1e
trynottosmile <marakhovsky.ivan@gmail.com>
2018-11-01 10:07:23 +0300

    colomn hides

--- Front/js/mainpage.js
+++ Front/js/mainpage.js
@@ -1,8 +1,21 @@
 
+function show(Id) {
+  var list = document.getElementsByClassName('arhiv');
+  for (var i = 0; i < list.length; i++) {
+    console.log(list[i].id); //second console output
+    hide(list[i].id);
+  }
+    var o=document.getElementById(Id);
+    o.style.display =  'inline';
+}
+function hide(Id){
+  var o=document.getElementById(Id);
+  o.style.display = 'none';
+}
     $(document).ready(function() {
             var h=$(this).height();
             $(".rowLeft").height(h);
             $(".nedeli").height(h);
-            console.log(h,h2);
+            console.log(h);
 
         });
