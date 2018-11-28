019c7baecd662a94c89462997ad84fcf1a0292d8
rikevoltz-dev-site <rikevoltz@yandex.ru>
2018-11-28 03:06:20 +0000

    Fix is email is in use in Django way

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from snowpenguin.django.recaptcha2.fields import ReCaptchaField
 from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 
+
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=100, label="Введите название курса")
     log = forms.FileField()
@@ -16,3 +17,8 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def is_valid(self):
+        if User.objects.filter(email=self.data.get('email')).count() != 0:
+            self.add_error('email', 'Данный адрес электронной почты уже используется')
+        return super(CustomUserCreationForm, self).is_valid()
