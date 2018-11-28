ef5970c1ce7243017bbee6a60d68a68656e8906f
rikevoltz-dev-site <rikevoltz@yandex.ru>
2018-11-28 23:28:17 +0000

    Fix redirect from login page, add redirect for register page if user has already logged in

--- mainapp/static/mainapp/js/main.js
+++ mainapp/static/mainapp/js/main.js
@@ -134,7 +134,7 @@ $(document).ready(function () {
     if ($(".is-already-authenticated").length){
         setTimeout(function () {
            $( location ).attr("href", '/');
-        }, 100000);
+        }, 1000);
     }
     var h = $(this).height();
     $(".row-courses-list").height(h);
