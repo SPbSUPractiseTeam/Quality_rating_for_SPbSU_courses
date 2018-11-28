c8e05fe69b66349e59b6172eef1048aa744cdfb3
rikevoltz-dev-site <rikevoltz@yandex.ru>
2018-11-28 03:08:33 +0000

    Remove old dirty solution

--- mainapp/views.py
+++ mainapp/views.py
@@ -72,8 +72,7 @@ def activate(request, uidb64, token):
         user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
-    if user is not None and not User.objects.filter(email=user.email).count() > 0 \
-            and account_activation_token.check_token(user, token):
+    if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
         return render(request, 'registration/message.html', {
